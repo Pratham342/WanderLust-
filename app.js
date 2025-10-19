e452ce53216d3690cbcb4fcef4f3b535f9dce870
Pratham Kul <kulshreshthapratham@gmail.com>
2025-10-19 23:57:38 +0530

    Setting root to /listings

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req,res) =>{
-//     res.send("working root");
-// });
+app.get("/", (req,res) =>{
+    res.redirect('/listings');
+});
 
 
 app.use(session(sessionOptions));
