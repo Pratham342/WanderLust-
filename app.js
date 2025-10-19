d052fba005b0a837ece76a8e291471391ec68cd9
Pratham Kul <kulshreshthapratham@gmail.com>
2025-10-19 23:47:35 +0530

    Used dynamic port for Render deployment

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ async function main() {
     await mongoose.connect(dbUrl);
 };
 
-let port = 8080;
+let port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
